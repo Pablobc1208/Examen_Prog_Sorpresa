9608223f99351624c9a4c1844ffc8c1b1314326d
antondm11 <antondm11@alumnos.ilerna.com>
2026-02-16 12:33:20 +0100

    Interface

--- ExamenSorpresa/src/examenSorpresa/Atacante.java
+++ ExamenSorpresa/src/examenSorpresa/Atacante.java
@@ -1,5 +1,12 @@
 package examenSorpresa;
 
 public interface Atacante {
-
+  
+	//Métodos a implementar
+	int calcularDano();
+	
+	void recibirDano (int puntos);
+	
+		
+	boolean estaOperativo();
 }
