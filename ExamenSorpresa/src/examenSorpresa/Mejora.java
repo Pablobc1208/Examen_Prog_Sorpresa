31826d845a572b5b07b9b0b5f9666c24db22e03f
antondm11 <antondm11@alumnos.ilerna.com>
2026-02-16 14:22:50 +0100

    Clase Mejora
    Added properties and constructor to Mejora class.

--- ExamenSorpresa/src/examenSorpresa/Mejora.java
+++ ExamenSorpresa/src/examenSorpresa/Mejora.java
@@ -1,5 +1,46 @@
 package examenSorpresa;
 
-public class Mejora {
+public class Mejora{
+
+	String descripcion;
+	int efecto;
+	int bonusAtaque;
+	int bonusDefensa;
+
+
+	//Constructor
+
+	public Mejora(String descripcion, int efecto, int bonusAtaque, int bonusDefensa) {
+		this.descripcion=descripcion;
+		this.efecto=efecto;
+
+	}
+
+
+	//Métodos
+
+	public void aplicarAUnidad(Unidad u) {
+
+	}
+
+	public void aplicarAEjercito(Ejercito u) {
+
+	}
+
+
+
+
+
+
+
+	//Mostrar info
+	@Override
+	public String consultarInfo() {
+
+		return "ID: " + id + "\nNombre: " + nombre + "\nAtaque base: " + ataqueBase + "\nDefensa base: " + defensaBase + "\nExperiencia: " + experiencia + "\nMultiplicadores: " + multiplicadores + "\nSalud Máxima: " + saludMaxima + "\nSalud Actual: " + saludActual;
+
+	}
+
 
 }
+
